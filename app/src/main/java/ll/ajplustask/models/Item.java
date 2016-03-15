1bd80ca589c16f77328473011ce2be9d65de064b
linle <linle.job@gmail.com>
2016-03-14 17:23:01 -0700

    make item Parcelable

--- app/src/main/java/ll/ajplustask/models/Item.java
+++ app/src/main/java/ll/ajplustask/models/Item.java
@@ -1,12 +1,15 @@
 package ll.ajplustask.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by Le on 2016/3/14.
  */
-public class Item {
+public class Item implements Parcelable {
     @SerializedName("id")
     @Expose
     public String id;
@@ -35,4 +38,41 @@ public class Item {
                 ", HLSURL='" + HLSURL + '\'' +
                 '}';
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.shortDescription);
+        dest.writeString(this.videoStillURL);
+        dest.writeInt(this.length);
+        dest.writeString(this.HLSURL);
+    }
+
+    public Item() {
+    }
+
+    protected Item(Parcel in) {
+        this.id = in.readString();
+        this.name = in.readString();
+        this.shortDescription = in.readString();
+        this.videoStillURL = in.readString();
+        this.length = in.readInt();
+        this.HLSURL = in.readString();
+    }
+
+    public static final Parcelable.Creator<Item> CREATOR = new Parcelable.Creator<Item>() {
+        public Item createFromParcel(Parcel source) {
+            return new Item(source);
+        }
+
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
 }
